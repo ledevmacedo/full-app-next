a724ae1ab2ba8704c07db4850b850febc298cc90
Leandro Macedo <leandro.smacedo@Live.com>
2025-10-20 14:28:35 +0100

    update schema

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,8 +1,12 @@
 import { URL_BASE_APP } from "@/constants"
 import { createAuthClient } from "better-auth/react"
+import { adminClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
-    baseURL: URL_BASE_APP
+    baseURL: URL_BASE_APP,
+    plugins: [
+        adminClient()
+    ]
 })
 
 export const { signIn, signUp, useSession } = authClient
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,14 @@
 import { betterAuth } from "better-auth";
+import { admin } from "better-auth/plugins"
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export const auth = betterAuth({
+    plugins: [
+        admin() 
+    ],
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
